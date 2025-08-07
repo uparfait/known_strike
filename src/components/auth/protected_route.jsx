f97cce17392cce39131ce7a2d427890eb9f48df4
Parfait Uwayo <167093044+uparfait@users.noreply.github.com>
2025-08-07 15:00:27 +0200

    Update protected_route.jsx

--- src/components/auth/protected_route.jsx
+++ src/components/auth/protected_route.jsx
@@ -4,8 +4,10 @@ import { Navigate } from 'react-router-dom'
 import { useAuth } from '../../contexts/auth_context'
 
 const ProtectedRoute = ({ children }) => {
-  const { is_authenticated } = useAuth()
-  
+  const { is_authenticated, loading } = useAuth()
+  if (loading) {
+    return <div className="flex items-center justify-center h-screen text-lg text-primary">Checking authentication...</div>
+  }
   return is_authenticated ? children : <Navigate to="/login" replace />
 }
 
